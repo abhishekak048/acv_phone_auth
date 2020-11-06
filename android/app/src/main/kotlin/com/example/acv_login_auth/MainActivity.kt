cd440a38db4f1e65a4d40688fb5d91a81ca771c0
Abhishek Kumar <abhishekak048@gmail.com>
2020-11-07 01:22:18 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/acv_login_auth/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.acv_login_auth
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:acv_login_auth/Login_screen/Login_screen.dart';
+import 'package:flare_splash_screen/flare_splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'ACV Login',
+      home: Animatation(),
+      theme: ThemeData(
+          brightness: Brightness.light,
+          scaffoldBackgroundColor: Colors.white,
+          primaryColor: Color(0xFFF2F3F7),
+          accentColor: Color(0xFFFFFFFF),
+          textSelectionColor: Color(0xFF707070)),
+      darkTheme: ThemeData.dark().copyWith(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.black,
+          backgroundColor: Colors.amber,
+          accentColor: Color(0xFFA8A6A6),
+          textSelectionColor: Color(0xFFFFFFFF)),
+    );
+  }
+}
+
+class Animatation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen.navigate(
+      name: "assets/ACV Funding.flr",
+      next: LoginScreen(),
+      width: double.infinity,
+      height: double.infinity,
+      alignment: Alignment.center,
+      until: () => Future.delayed(Duration(microseconds: 200)),
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      startAnimation: "ACV",
+    );
+  }
+}
